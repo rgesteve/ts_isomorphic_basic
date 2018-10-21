d67231521f5c011eeda79ef558a9ba50eb1411f0
R. G. Esteves <rgesteves@gmail.com>
2018-10-20 22:08:31 -0700

    fixed header

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,7 +5,7 @@ import  {serverPort} from './config';
 console.log(`Running server from directory ${__dirname}.  I'm thinking of using port ${serverPort}.`);
 
 let server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-type':'text/plan'});
+    res.writeHead(200, {'Content-type':'text/plain'});
     res.write('Hello Node JS Server Response');
     res.end( );
 }).listen(serverPort);
