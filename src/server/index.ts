916dfe1a95638efd882cc5cda99c395ce704db3d
R. G. Esteves <rgesteves@gmail.com>
2018-10-20 21:52:57 -0700

    Broken up in modules and fixed module export

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,6 @@
-import 'http';
+import * as http from 'http';
+import  {serverPort} from './config';
 
-console.log(`Testing from directory ${__dirname}.`);
+console.log(`Running server from directory ${__dirname}.  I'm thinking of using port ${serverPort}.`);
+
+// let server = http.createServer(() => {}).listen();
