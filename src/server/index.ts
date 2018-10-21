e690a70906633824ef71f4311f8860a3e850f834
R. G. Esteves <rgesteves@gmail.com>
2018-10-20 22:22:11 -0700

    Serving file

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,24 @@
 import * as http from 'http';
 import * as fs from 'fs';
+import * as path from 'path';
 import  {serverPort} from './config';
 
 console.log(`Running server from directory ${__dirname}.  I'm thinking of using port ${serverPort}.`);
+let indexHtml = path.join(__dirname,'..', 'src', 'client', 'index.html');
+if (fs.existsSync(indexHtml)) {
+    console.log("Found the file, should be serving it...");
+} else {
+    console.log(`Didn't find file ${indexHtml}, deeper problems somewhere...`);
+}
 
 let server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-type':'text/plain'});
-    res.write('Hello Node JS Server Response');
-    res.end( );
+    res.writeHead(200, {'Content-type':'text/html'});
+    res.write(fs.readFileSync(indexHtml));
+    /*
+   res.writeHead(200, "text/plain");
+   res.write("Testing the server...!");
+   */
+   res.end();
 }).listen(serverPort);
 
 console.log(`Done!`);
