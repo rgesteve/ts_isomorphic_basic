d6decef6b91e5cf715a0b843c8ea1791303a3b79
R. G. Esteves <rgesteves@gmail.com>
2018-10-27 19:55:24 -0700

    A very simple user-provided library

--- src/client/index.ts
+++ src/client/index.ts
@@ -14,8 +14,13 @@ $(() => {
 });
 */
 
+import { Module1 } from './lib/module1';
+
 export class ClientTest {
     static start() {
         console.log("Testing from index.ts, now in a static function");
+        let m1 = new Module1();
+        m1.print();
+        console.log("Now tested instance from user-provided library");
     }
 }
